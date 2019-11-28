8490aefed0dc150eea986040f50321d3792d7952
Yozshura3 <julien.miralles@lapiscine.pro>
2019-11-28 23:45:38 +0100

    second commit

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    /** Déclaration de la page d'accueil
+     * @Route("/", name="accueil")
+     */
+    public function accueilController(){
+        return $this->render('accueil.html.twig');
+    }
+
+
+
+
+
+
+
+
+}
